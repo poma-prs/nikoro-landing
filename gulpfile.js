5d8a28f2d98ac85895bef0495f7fea62e5bc0ad4
poma.prs <poma.prs@gmail.com>
2018-01-05 12:05:56 +0400

    init

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var fs = require('fs');
+
+/**
+ *  This will load all js or coffee files in the gulp directory
+ *  in order to load all gulp tasks
+ */
+fs.readdirSync('./gulp').filter(function(file) {
+  return (/\.(js|coffee)$/i).test(file);
+}).map(function(file) {
+  require('./gulp/' + file);
+});
